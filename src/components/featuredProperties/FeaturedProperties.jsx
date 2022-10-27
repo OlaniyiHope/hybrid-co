70df990c70f6820d5c44d0f95efa2d650cf7c18d
OlaniyiHope <olaniyihoppee@gmail.com>
2022-10-27 08:46:00 +0100

    added proxy

--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -16,23 +16,23 @@ const FeaturedProperties = () => {
         ) : (
           <>
             {data &&
-              data.map((data) => (
-                <div className="fpItem" key={data?._id}>
-                  <Link to={`/hotels/${data?._id}`}>
-                    <img src={data?.photos[0]} alt="" className="fpImg" />
+              data.map((item) => (
+                <div className="fpItem" key={item?._id}>
+                  <Link to={`/hotels/${item?._id}`}>
+                    <img src={item?.photos[0]} alt="" className="fpImg" />
                   </Link>
                   <div className="fpTitle">
-                    <span className="fpName">{data?.name}</span>
+                    <span className="fpName">{item?.name}</span>
                     <br></br>
                     <span className="fpCity">
                       <bold>
                         <FontAwesomeIcon icon={faLocationDot} />
-                        {data?.city}
-                      </bold>{" "}
+                        {item?.city}
+                      </bold>
                     </span>
                     <br></br>
                     <span className="fpPrice">
-                      <bold>Price: N{data?.cheapestPrice}</bold>
+                      <bold>Price: N{item?.cheapestPrice}</bold>
                     </span>
                     <br></br>
                   </div>
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -11,8 +11,7 @@ const useFetch = (url) => {
       setLoading(true);
       try {
         const res = await axios.get(url);
-        //setData(res.data);
-        if (res.data) setData(res.data);
+        setData(res.data);
       } catch (err) {
         setError(err);
       }
